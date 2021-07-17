1824353e835c21dc076cff448574a840219ed130
engrmazharjan <mazharj4565gmail.com>
2021-07-17 10:06:49 +0500

    Installing & Setting Up Nodemon

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const dotenv = require("dotenv");
 
 // Setting Up Config File
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: ".env" });
 app.listen(process.env.PORT, () => {
   console.log(
     `Server Started On PORT:${process.env.PORT} in ${process.env.NODE_ENV} mode`
