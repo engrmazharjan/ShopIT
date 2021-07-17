b3f73401ff854eb50897b8b6902d2cc95acb2850
engrmazharjan <mazharj4565gmail.com>
2021-07-17 10:19:22 +0500

    Creating First Route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
+// Import All Routes
+const productRoutes = require("./routes/productRoutes");
+
+app.use("/api/v1", productRoutes);
 module.exports = app;
