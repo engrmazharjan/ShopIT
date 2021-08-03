85dea48f2380f51d8026099d7c1b618af7e89845
engrmazharjan <mazharj4565gmail.com>
2021-08-03 15:50:54 +0500

    All Products Action

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,33 @@
+import {
+  ALL_PRODUCTS_FAIL,
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  CLEAR_ERRORS,
+} from "../constants/productConstants";
+
+import axios from "axios";
+
+export const getAllProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: ALL_PRODUCTS_REQUEST });
+
+    const { data } = await axios.get("/api/v1/products");
+
+    dispatch({
+      type: ALL_PRODUCTS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_PRODUCTS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Clear Errors
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
