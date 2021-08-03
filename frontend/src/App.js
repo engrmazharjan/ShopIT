5970c8b4f5b6c2b1bcc944ffe226650770d0add2
engrmazharjan <mazharj4565gmail.com>
2021-08-03 12:41:43 +0500

    ShopIT HomePage/HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
+import HomeScreen from "./components/HomeScreen";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeScreen />
       <Footer />
     </div>
   );
--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,8 +3,8 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <footer className="py-5">
-        <p className="text-center mt-5 py-5">
+      <footer className="py-1">
+        <p className="text-center mt-1">
           Shopping Cart - 2021, &copy; All Rights Reserved: Mazhar Jan <br />{" "}
           Copyright &copy; ShopIT
         </p>
